c9ac5f4c87c4b46ebec5f92588601650b4df0de4
Roman Bulych <rbulych@Romans-MacBook-Pro.local>
2025-10-14 12:25:33 +0300

    Remove old comments

--- routes/web.php
+++ routes/web.php
@@ -6,32 +6,5 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/', function () {
     return Inertia::render('Index');
 })->name('home');
-
-/*Route::get('/login', function () {
-    return Inertia::render('Login');
-})->name('login');
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');*/
-
-/*Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});*/
-
-//require __DIR__.'/auth.php';
